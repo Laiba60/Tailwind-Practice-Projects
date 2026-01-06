000b6fb495f10934337d8d92cb40a3fe911fae21
Laiba60 <laibasaeed026@gmail.com>
2026-01-06 00:43:57 -0800

    Made the Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import Banner from '../assets/images/Banner.jpg'
+import Logo from '../assets/images/Logo.avif'
 
 const Header = () => {
+  const menuItems = [
+    "About",
+    "Products",
+    "Plants",
+    "Price",
+    "Gallery",
+    "Animation",
+    "Specification",
+    "Contact Us"
+  ]
+
   return (
-    <section className="w-full ">
-      <div className="max-w-700 mx-auto">
+    <section
+      className="bg-cover bg-center w-full h-screen"
+      style={{ backgroundImage: `url(${Banner})` }}
+    >
+      <header className="py-4">
+        <div className="max-w-[1240px] mx-auto flex justify-between items-center px-40">
+          
+          {/* Logo */}
+          <figure>
+            <img src={Logo} alt="Logo" className="w-15 h-auto" />
 
-      </div>
+          </figure>
 
+          {/* Navigation */}
+          <nav>
+            <ul className="flex items-center gap-8">
+              {menuItems.map(item => (
+                <li key={item}>
+                  <a
+                    href="#"
+                    className="text-white text-[15px] font-[var(--font-jost)] hover:text-green-400 transition"
+                  >
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+
+        </div>
+      </header>
     </section>
   )
 }
